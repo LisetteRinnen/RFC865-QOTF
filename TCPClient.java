85d1a69e705e8c7d165b4993745904d5df3224d7
LisetteRinnen <77075034+LisetteRinnen@users.noreply.github.com>
2023-04-13 17:05:16 -0700

    Update TCPClient.java

--- TCPClient.java
+++ TCPClient.java
@@ -8,12 +8,6 @@ public class TCPClient {
     int port = Integer.valueOf(17);
 
     try (Socket socket = new Socket(host, port)) {
-      // send message and connect with server
-      OutputStream output = socket.getOutputStream();
-      String message = "QOTD please";
-      output.write(message.getBytes());
-      System.out.println("Request sent!");
-
       // recieve quote and print
       InputStream input = socket.getInputStream();
       int exists = input.read();
